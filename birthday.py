2d823692a16c188b67b0c87839245bbdc4e9190b
willl4 <williamlaycock@hanovernorwichschools.org>
2017-02-06 14:59:15 -0500

    Updated from Brython Server: February 6, 2017 2:59:15 PM EST

--- birthday.py
+++ birthday.py
@@ -42,18 +42,18 @@ year = input("And what year were you born in, {0}? ".format(name))
 day = input("And the day? ")
 month = month.lower()
 
-if month is december or january or february:
+if month == "december" or month == "january" or month == "february":
     season = winter
-elif month is march or april or may:
+elif month == "march" or month == "april" or month == "may":
     season = spring
-elif month is june or july or august:
+elif month == "june" or month == "july" or month == "august":
     season = summer
-elif month is september or october or november:
+elif month == "september" or month == "october" or month == "november":
     season = fall
 
 if year is <= 1980:
     era = stone age
-elif year is 
+elif year is >= 1980 and 
 
 
 
